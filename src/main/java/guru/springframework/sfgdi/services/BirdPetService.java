c224240ed4860a0338123f32fd889b5435687736
Paweł Sowa <pawel.sowa.00@gmail.com>
2021-05-03 12:20:03 +0200

    pet profiles

--- src/main/java/guru/springframework/sfgdi/services/BirdPetService.java
+++ src/main/java/guru/springframework/sfgdi/services/BirdPetService.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/28/19.
  */
-@Profile({"dog", "default"})
-@Service("pet")
-public class DogPetService implements PetService{
-    public String getPetType(){
-        return "Dogs are the best!";
+@Service("birds")
+public class BirdPetService implements PetService {
+    @Override
+    public String getPetType() {
+        return "Birds Are the Best!";
     }
 }
